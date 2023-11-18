8bdb303176e87b8d611cb467cabaefbdbd3e5707
alvaroescudero7 <136207410+alvaroescudero7@users.noreply.github.com>
2023-11-18 14:43:05 -0500

    Creación de constructor, getters y setters de la clase Autor

--- src/fb/model/Autor.java
+++ src/fb/model/Autor.java
@@ -1,5 +1,57 @@
 package fb.model;
 
 public class Autor {
+    private String id;
+    private String nombres;
+    private String apellidos;
+    private String nacionalidad;
+    private String estado;
 
+    public Autor(String id, String nombres, String apellidos, String nacionalidad) {
+        this.id = id;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.nacionalidad = nacionalidad;
+        this.estado = "ACTIVO";
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
